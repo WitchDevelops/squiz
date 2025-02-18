16d299cf3fd08b319bea0455286a97a3e3cdcdba
WitchDevelops <d.n.wojewska@gmail.com>
2025-02-18 22:54:04 +0100

    refactor: card styles

--- src/components/CompanyCard.tsx
+++ src/components/CompanyCard.tsx
@@ -14,13 +14,13 @@ export const CompanyCard: React.FC<CompanyCardProps> = ({
   numberOfEmployees,
 }) => {
   return (
-    <div className="shadow-md rounded-xl p-4 hover:shadow-lg flex flex-col items-center justify-center gap-6 min-h-[250px] text-center text-balance">
-      <div className="flex flex-col items-center gap-3 w-full">
-        <p className="font-bold text-4xl">{name}</p>
-        <p className="text-gray-500 uppercase text-sm text-center">{industry}</p>
+    <div className="shadow-md rounded-xl hover:shadow-lg flex flex-col items-center justify-between text-center text-balance overflow-hidden">
+      <div className="flex flex-col items-center gap-3 w-full bg-blue-950 py-4 px-2">
+        <p className="font-bold text-4xl text-yellow-500">{name}</p>
+        <p className="text-yellow-500 uppercase text-sm text-center">{industry}</p>
       </div>
 
-      <div className="grid grid-cols-2 gap-4 w-full">
+      <div className="grid xs:grid-cols-1 sm:grid-cols-2 gap-4 w-full py-4 min-h-[100px] h-full bg-slate-50">
         <div className="flex flex-col gap-1 items-center">
           <p className="text-gray-500 uppercase text-sm">Country</p>
           <p className="text-foreground font-bold text-xl">{country}</p>

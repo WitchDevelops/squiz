cc5436aec14e5b17ad5830f749ab04c1f14e19be
WitchDevelops <d.n.wojewska@gmail.com>
2025-02-22 18:50:34 +0100

    add react-tooltip

--- src/components/NoDataFound.tsx
+++ src/components/NoDataFound.tsx
@@ -1,5 +1,6 @@
 import { Cloud, SearchX } from "lucide-react";
 import { FiltersType } from "@/types/types";
+import { Tooltip } from "react-tooltip";
 
 export const NoDataFound = ({ setFilters }: { setFilters: (filters: FiltersType) => void }) => {
   const handleRefreshPage = () => window.location.reload();
@@ -9,14 +10,24 @@ export const NoDataFound = ({ setFilters }: { setFilters: (filters: FiltersType)
     <div className="flex-1 min-h-[50vh] flex flex-col gap-20 justify-center items-center mx-auto w-full">
       <p className="text text-center">No companies found for the given filters.</p>
       <div className="flex flex-col md:flex-row items-center gap-4 w-full justify-center">
-        <button className="no-results-button" onClick={handleResetFilters}>
+        <button
+          className="no-results-button"
+          onClick={handleResetFilters}
+          data-tooltip-id="clear-filters-btn-tooltip"
+        >
           <SearchX size={50} color="var(--color-accent)" />
           Clear the filters and try again
         </button>
-        <button className="no-results-button" onClick={handleRefreshPage}>
+        <Tooltip id="clear-filters-btn-tooltip">Reset the filters</Tooltip>
+        <button
+          className="no-results-button"
+          onClick={handleRefreshPage}
+          data-tooltip-id="refresh-btn-tooltip"
+        >
           <Cloud size={50} color="var(--color-accent)" />
           Yell at cloud
         </button>
+        <Tooltip id="refresh-btn-tooltip">Refresh the page</Tooltip>
       </div>
     </div>
   );
--- src/components/ViewSwitch.tsx
+++ src/components/ViewSwitch.tsx
@@ -1,4 +1,5 @@
 import { List, LayoutGrid as Grid } from "lucide-react";
+import { Tooltip } from "react-tooltip";
 
 interface ViewSwitchProps {
   isListView: boolean;
@@ -7,13 +8,17 @@ interface ViewSwitchProps {
 
 export const ViewSwitch = ({ isListView, onToggle }: ViewSwitchProps) => {
   return (
-    <div className="flex items-center space-x-2">
+    <>
       <button
         className="p-2 rounded-md bg-slate-50 dark:bg-slate-700 shadow-md hover:bg-slate-200 dark:hover:bg-slate-600 active:bg-slate-300 dark:active:bg-slate-500 focus:outline-2 focus:outline-offset-2 focus:outline-primary"
         onClick={onToggle}
+        data-tooltip-id="view-tooltip"
       >
         {isListView ? <Grid size={20} /> : <List size={20} />}
       </button>
-    </div>
+      <Tooltip id="view-tooltip" place="bottom">
+        {isListView ? "Switch to grid view" : "Switch to list view"}
+      </Tooltip>
+    </>
   );
 };
